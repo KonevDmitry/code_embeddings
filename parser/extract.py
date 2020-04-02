9b78196728acc429a45e1f04a241233160d55c15
DeadmanIQ445 <solovov305@gmail.com>
2020-04-02 15:08:40 +0300

    fix

--- parser/extract.py
+++ parser/extract.py
@@ -24,7 +24,7 @@ parser.add_argument('--max_path_length', type=int, default=8)
 parser.add_argument('--max_path_width', type=int, default=2)
 parser.add_argument('--use_method_name', type=bool, default=True)
 parser.add_argument('--use_nums', type=bool, default=True)
-parser.add_argument('--output_file', type=str, default='./extract_res')
+parser.add_argument('--output_file', type=str, default='../resources/extract_res')
 parser.add_argument('--n_jobs', type=int, default=multiprocessing.cpu_count())
 parser.add_argument('--seed', type=int, default=239)
 
